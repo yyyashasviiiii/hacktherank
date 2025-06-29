527e9ceab9fc798dbe5c8a15af5e77dbf0246899
Yashasvi Shukla <yashasvigot@gmail.com>
2025-06-29 22:13:48 +0530

    Hello World solution

---
+++ python/Say "Hello, World!" With Python.py
@@ -0,0 +1,22 @@
+"""
+Problem: Say "Hello, World!" With Python
+Link: https://www.hackerrank.com/challenges/py-hello-world/problem
+
+Description:
+This is a simple warm-up exercise to get started with Python.
+
+Task:
+- Print "Hello, World!" to standard output using Python's `print()` function.
+- You may store the string in a variable or print it directly.
+
+Input Format:
+- There is no input for this challenge.
+
+Output Format:
+- Print the exact string: Hello, World!
+
+Sample Output:
+Hello, World!
+"""
+
+print("Hello, World!")
